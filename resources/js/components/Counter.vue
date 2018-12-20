5b32cfd61184b14a2f0666a49846d56be5ac1218
burak <buryo95@gmail.com>
2018-12-20 06:03:04 +0100

    first commit

---
+++ resources/js/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2 v-text="countFrom" @click="countDown"></h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "counter",
+        data() {
+            return {
+                countFrom: 10
+            }
+        },
+        methods: {
+            countDown () {
+                this.countFrom -= 1
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="header bg-cover bg-center bg-no-repeat relative">
+        <div class="absolute pin-t pin-l h-full w-full bg-black-lighter opacity-25"></div>
+        <div class="container mx-auto">
+            <div class="content h-80 md:h-112 xl:h-160 flex flex-col items-center justify-center sticky">
+                <h1 class="select-none z-10 text-white uppercase text-3xl md:text-5xl lg:text-7xl header-title mb-4">Board Game</h1>
+                <p class="select-none z-10 text-1xl md:text-3xl lg:text-4xl tracking-wide text-white">A Project For University Windesheim</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "b-header"
+    }
+</script>
+
+<style scoped>
+
+</style>
